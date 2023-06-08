87b59825dd04708577a5d197d76c187185179db6
LeeNaYeon <ny_lee0121@naver.com>
2023-06-08 17:15:57 +0900

    제곱커밋

--- main.py
+++ main.py
@@ -12,6 +12,9 @@ def mul_func(n1,n2):
 def div_func(n1,n2):
     return n1 / n2
 
+def dub_func(n1,n2):
+    return n1 ** n2
+
 ## 전역 변수부
 num1, num2, res = 100, 200, 0
 
@@ -28,3 +31,6 @@ print(num1, "*", num2, "=", res)
 
 res = div_func(num1,num2)
 print(num1, "/", num2, "=", res)
+
+res = dub_func(num1,num2)
+print(num1, "^", num2, "=", res)
