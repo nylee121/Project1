24cf7485dcb25655da131d224f235f1fe59a681f
LeeNaYeon <ny_lee0121@naver.com>
2023-06-08 14:57:09 +0900

    커밋 3 곱하기 추가...

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ def add_func(n1,n2):
 def sub_func(n1,n2):
     return n1 - n2
 
+def mul_func(n1,n2):
+    return n1 * n2
 
 ## 전역 변수부
 num1, num2, res = 100, 200, 0
@@ -17,3 +19,6 @@ print(num1, "+", num2, "=", res)
 
 res = sub_func(num1,num2)
 print(num1, "-", num2, "=", res)
+
+res = mul_func(num1,num2)
+print(num1, "*", num2, "=", res)
